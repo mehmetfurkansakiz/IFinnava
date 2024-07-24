cee82fae52eee161d079be7167187cdb3e10023d
mehmetfurkansakiz <mehmetfurkansakiz@gmail.com>
2024-07-24 23:04:54 +0300

    Initial Commit

---
+++ IFinnava/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IFinnava
+//
+//  Created by furkan sakız on 24.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
