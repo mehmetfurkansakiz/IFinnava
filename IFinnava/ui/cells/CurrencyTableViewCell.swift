feb51bdc068ec94587bc1f0ad24460a9ca1b8d5a
mehmetfurkansakiz <mehmetfurkansakiz@gmail.com>
2024-07-25 11:41:07 +0300

    Firebase login added.

---
+++ IFinnava/ui/cells/CurrencyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  IFinnava
+//
+//  Created by furkan sakız on 25.07.2024.
+//
+
+import UIKit
+
+class CurrencyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ IFinnava/ui/viewmodels/CurrencyViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CurrencyViewModel.swift
+//  IFinnava
+//
+//  Created by furkan sakız on 25.07.2024.
+//
+
+import Foundation
