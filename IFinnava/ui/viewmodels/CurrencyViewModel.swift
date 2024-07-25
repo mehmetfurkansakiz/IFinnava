ef6d63efe9eec3c4cefbf9e36aac7ccdae71e98e
mehmetfurkansakiz <mehmetfurkansakiz@gmail.com>
2024-07-25 13:38:24 +0300

    CurrencyViewModel added with rxswift.

--- IFinnava/ui/viewmodels/CurrencyViewModel.swift
+++ IFinnava/ui/viewmodels/CurrencyViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import RxSwift
+import UIKit
+
+class CurrencyViewModel {
+    
+    var currencyRepo = CurrencyDaoRepository()
+    var currencyList = BehaviorSubject<[Currency]>(value: [Currency]())
+    
+    init() {
+        currencyList = currencyRepo.currencyList
+    }
+    
+    func getCurrencies() {
+        currencyRepo.getCurrencies()
+    }
+}
