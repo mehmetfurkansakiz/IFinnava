f2fdcecbdfb77dbabd96d19cd5d49812ab28c297
mehmetfurkansakiz <mehmetfurkansakiz@gmail.com>
2024-07-25 13:37:11 +0300

    alert helper added to helpers.

--- IFinnava/helpers/AlertHelper.swift
+++ IFinnava/helpers/AlertHelper.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import UIKit
+
+class AlertHelper {
+    static func basicAlert(title: String, message: String, in viewController: UIViewController) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        viewController.present(alert, animated: true)
+    }
+}
